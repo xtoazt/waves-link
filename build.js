566e65aa02e22d136cc366a12997ad48640c203b
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-12 14:03:34 -0400

    Update build.js

--- build.js
+++ build.js
@@ -7,14 +7,14 @@ async function setup() {
         console.log("Anura already installed")
     } catch(e) {
         console.log("Anura static not installed, downloading")
-        await fs.writeFile("static.zip", Buffer.from(await (await (fetch("https://github.com/MercuryWorkshop/anuraOS/releases/download/latest/anura.zip"))).arrayBuffer())); // Insanity
+        await fs.writeFile("static.zip", Buffer.from(await (await (fetch("https://github.com/xojw/waves/archive/refs/tags/v2.0.0.zip"))).arrayBuffer())); // Insanity
         await fs.mkdir("static");
         console.log("Extracting to " + __dirname + "/static")
         await extract("static.zip", { dir: __dirname + "/static" });
 
         const config = JSON.parse(await fs.readFile(__dirname + "/static/config.json", "utf-8"));
         // config chanegs
-        config.defaultsettings["wisp-url"] = "wss://anura.pro/"; // Wisp server in lieu of vercel supporting one
+        config.defaultsettings["wisp-url"] = "wss://usewaves.site/w/"; // Wisp server in lieu of vercel supporting one
         fs.writeFile(__dirname + "/static/config.json", JSON.stringify(config)); // save config
     }
 }
